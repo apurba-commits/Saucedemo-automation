54a433a5d7370041d6b42425a4427035251faa8f
apurba1401 <apurba1401@gmail.com>
2025-07-16 13:14:04 +0530

    add files

--- conftest.py
+++ conftest.py
@@ -3,10 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from utils.screenshot import take_screenshot
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode on CI
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")  # Optional: better stability
+    chrome_options.add_argument("--window-size=1920,1080")  # Optional: full window
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     yield driver
     driver.quit()
 
